ca3a8ad533b9d8cfcc0a7d54cc3e716404b2f987
Joshua Tokle <jtokle@air.org>
2015-03-02 11:48:52 -0500

    Restructure make_doc.py into self-contained functions

--- querymodule/doc/make_doc.py
+++ querymodule/doc/make_doc.py
@@ -1,10 +1,15 @@
-import markdown, string
+import markdown, openpyxl, os, string, sys
 
 import xml.etree.ElementTree as ET
 
-CONTENT_FILE = "doc.md"
+OUTPUT_DIRECTORY = "test"
 
-def main():
+
+def make_field_list_json(outdir):
+    pass
+
+
+def make_documentation_html(outdir):
     tpl = string.Template("""
           <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
               "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -20,10 +25,17 @@ def main():
           </html>
           """)
 
-    md = open(CONTENT_FILE).read()
+    md = open("doc.md").read()
     html = markdown.markdown(md)
 
-    print(tpl.substitute(content=html))
+    with open(os.path.join(outdir, "doc.html")) as f:
+        print(tpl.substitute(content=html), file=f)
+
+
+def main():
+    make_field_list_json(OUTPUT_DIRECTORY)
+    make_documentation_html(OUTPUT_DIRECTORY)
+
 
 if __name__ == "__main__":
     main()
