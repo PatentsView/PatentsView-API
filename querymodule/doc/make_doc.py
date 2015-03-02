cadfa2a0ad5b1deb51f3ab6ba7546aff5300bb26
Joshua Tokle <jtokle@air.org>
2015-03-02 14:05:19 -0500

    update documentation generator to run off of field list spreadsheet

--- querymodule/doc/make_doc.py
+++ querymodule/doc/make_doc.py
@@ -1,12 +1,39 @@
-import markdown, openpyxl, os, string, sys
+import json, markdown, openpyxl, os, re, string, sys
 
 import xml.etree.ElementTree as ET
 
-OUTPUT_DIRECTORY = "test"
+OUTPUT_DIRECTORY = "../public_html"
 
 
-def make_field_list_json(outdir):
-    pass
+def sheet_data(ws):
+    ary = []
+    header = None
+
+    for row in ws.iter_rows():
+        values = [cell.value for cell in row]
+
+        if not header:
+            header = values
+            continue
+
+        if not any(values):
+            continue
+
+        ary.append(dict(zip(header, values)))
+
+    return ary
+
+
+def make_field_list_html(field_list):
+
+    def field_list_columns(row):
+        column_names = ["API Field Name", "Group", "Type", "Query", "Return", "Sort"]
+        cols = [(re.sub(" ", "_", s.lower()), s) for s in column_names]
+
+        return dict((k, row[v]) for k, v in cols)
+
+    s = "<tr><td>{api_field_name}</td><td>{group}</td><td>{type}</td><td>{query}</td><td>{return}</td><td>{sort}</td></tr>"
+    return "\n".join(s.format(**field_list_columns(row)) for row in field_list)
 
 
 def make_documentation_html(outdir):
@@ -25,17 +52,42 @@ def make_documentation_html(outdir):
           </html>
           """)
 
-    md = open("doc.md").read()
-    html = markdown.markdown(md)
+    field_lists = {}
+    wb = openpyxl.load_workbook("API field lists.xlsx", data_only=True, use_iterators=True)
 
-    with open(os.path.join(outdir, "doc.html")) as f:
-        print(tpl.substitute(content=html), file=f)
+    if not os.path.exists(os.path.join(outdir, "field_lists")):
+        os.makedirs(os.path.join(outdir, "field_lists"))
+
+    keep_sections = ["patent", "inventor", "assignee", "cpc subsection", "uspc", "nber subcat", "location"]
+
+    for ws in wb.worksheets:
+        if ws.title not in keep_sections:
+            continue
+
+        result = sheet_data(ws)
+        field_lists[ws.title] = make_field_list_html(result)
+
+        with open(os.path.join(outdir, "field_lists", "{}.json".format(ws.title)), "w") as f:
+            print(json.dumps(result), file=f)
+
+    md = string.Template(open("doc.md").read())
+    md2 = md.substitute(patent_field_list=field_lists["patent"],
+                        inventor_field_list=field_lists["inventor"],
+                        assignee_field_list=field_lists["assignee"],
+                        cpc_subsection_field_list=field_lists["cpc subsection"],
+                        uspc_field_list=field_lists["uspc"],
+                        nber_subcat_field_list=field_lists["nber subcat"],
+                        location_field_list=field_lists["location"])
+    html = markdown.markdown(md2)
+
+    with open(os.path.join(outdir, "doc.html"), "w") as f:
+        s = tpl.substitute(content=html)
+        print(s, file=f)
 
 
-def main():
-    make_field_list_json(OUTPUT_DIRECTORY)
-    make_documentation_html(OUTPUT_DIRECTORY)
+def main(outdir):
+    make_documentation_html(outdir)
 
 
 if __name__ == "__main__":
-    main()
+    main(OUTPUT_DIRECTORY)
