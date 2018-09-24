bf5a563126747dde5f2a5b6525c30c2abacd1670
Sarvothaman Madhavan <smadhavan@air.org>
2018-09-24 17:54:21 -0400

    Swithched to json format

--- querymodule/app/entitySpecs.php
+++ querymodule/app/entitySpecs.php
@@ -35,12 +35,15 @@ $INVENTOR_ENTITY_SPECS = json_decode($inventor_entity_json, true);
 $inventor_field_json = file_get_contents(dirname(__FILE__) . '/specs/inventor-field-specs.json');
 $INVENTOR_FIELD_SPECS = json_decode($inventor_field_json, true);
 
+<<<<<<< HEAD
 
 $assignee_entity_json = file_get_contents(dirname(__FILE__) . '/specs/assignee-entity-specs.json');
 $ASSIGNEE_ENTITY_SPECS = json_decode($assignee_entity_json, true);
 $assignee_field_json = file_get_contents(dirname(__FILE__) . '/specs/assignee-field-specs.json');
 $ASSIGNEE_FIELD_SPECS = json_decode($assignee_field_json, true);
 
+=======
+>>>>>>> Swithched to json format
 
 $cpcgroup_entity_json = file_get_contents(dirname(__FILE__) . '/specs/cpc-group-entity-specs.json');
 $CPC_GROUP_ENTITY_SPECS = json_decode($cpcgroup_entity_json, true);
