a432c0553d3f6cba169e06efea157f60edaf5789
(Noah) Hanlun Song <hsong@air.org>
2019-09-24 12:21:54 -0400

    implemented header component

---
+++ header_footer_src/icons/IconBase.vue
@@ -0,0 +1,49 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" 
+    :width="width" 
+    :height="height" 
+    viewBox="50 -30 400 400" 
+    :aria-labelledby="iconName" 
+    role="presentation"
+    :fill="color"
+    :stroke="stroke"
+  >
+    <title :id="iconName" lang="en">{{iconName}} icon</title>
+    <slot />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+      default: 'box'
+    },
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 18
+    },
+    color: {
+      type: String,
+      default: '#9cabb9'
+    },
+    stroke: {
+      type: String,
+      default: '#9cabb9'
+    }
+  }
+}
+</script>
+
+<style scoped>
+svg {
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 5px;
+}
+</style>
