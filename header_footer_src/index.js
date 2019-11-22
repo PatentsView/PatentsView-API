5349a02a975ab00afb0e6eb05fc4979bab5af2a6
(Noah) Hanlun Song <hsong@air.org>
2019-11-22 12:19:46 -0500

    Add temporary SSH notification

--- header_footer_src/index.js
+++ header_footer_src/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue"
-import Header from "./components/header.vue"
-import Footer from "./components/footer"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+// temporary banner
+import SSHNotif from './components/SSHNotif'
 
 const header = document.createElement("div")
 document.body.prepend(header)
@@ -16,4 +19,12 @@ document.body.append(footer)
 new Vue({
     el: footer,
     render: h => h(Footer)
+})
+
+const sshNotif = document.createElement("div")
+document.body.append(sshNotif)
+
+new Vue({
+    el: sshNotif,
+    render: h => h(SSHNotif)
 })
